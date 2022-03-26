1a6f64a448d40aa5ff5a66ac99eb1c99cb5cf190
iktovr <vikvladbir@mail.ru>
2022-03-26 14:45:26 +0300

    Change font and indents

--- lab1/lab1.lisp
+++ lab1/lab1.lisp
@@ -1,23 +1,23 @@
 ; 1.45
 
 (defun cube (x)
-	(* x x x))
+        (* x x x))
 
 (defun square (x)
-	(* x x))
+        (* x x))
 
 (defun improve (guess x)
-	(/ (+ (/ x (square guess)) (* 2 guess)) 3))
+        (/ (+ (/ x (square guess)) (* 2 guess)) 3))
 
 (defconstant eps 0.0001)
 
 (defun good-enough-p (guess x)
-	(< (abs (- (cube guess) x)) eps))
+        (< (abs (- (cube guess) x)) eps))
 
 (defun cuberoot-iter (guess x)
-	(if (good-enough-p guess x)
-		guess
-		(cuberoot-iter (improve guess x) x)))
+        (if (good-enough-p guess x)
+                guess
+                (cuberoot-iter (improve guess x) x)))
 
 (defun cuberoot (x)
-	(cuberoot-iter 1.0 x))
+        (cuberoot-iter 1.0 x))
